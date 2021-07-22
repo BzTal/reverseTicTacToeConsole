b9c4cd1a807bf9afabb0ff17dfd1d00f36336fc8
Talbe <Talbe@LAPTOP-OUL0NM6Q>
2021-07-22 15:58:45 +0300

    Add project files.

---
+++ CSharpSecondAssigment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace B21_Ex02
+{
+   public class Program
+    {
+        public static void Main()
+        {
+            UserInterface UI = new UserInterface();
+            UI.RunProgram();
+        }
+    }
+}
